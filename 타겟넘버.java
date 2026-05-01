143324f879a5349dbe0fc74d16b48e6464317148
배준석 <qownstjr77@gmail.com>
2026-05-01 22:59:33 +0900

    programmers 타겟넘버

--- 타겟넘버.java
+++ 타겟넘버.java
@@ -16,18 +16,26 @@ public class 타겟넘버 {
     public int solution(int[] numbers, int target) {
         int answer = 0;
         // 제일 작게 나올 수 있는 값이랑, 제일 크게 나올 수 있는 값으로 범위를 제한
-        int sum = 0;
-        for(int i = 0; i < numbers.length; i++){
-            sum += numbers[i];
-        }
-        boolean[] isVisited = new boolean[2 * sum + 1];
-        Queue<Integer> q = new LinkedList<>();
-        q.add(numbers[0]);
-        q.add(numbers[1] * (-1));
-        isVisited[numbers[0] + sum] = true;
-        isVisited[numbers[0] * (-1) + sum] = true;
+        Queue<int[]> q = new LinkedList<>();
+        q.add(new int[]{0, numbers[0]});
+        q.add(new int[]{0, numbers[0] * (-1)});
         while (!q.isEmpty()) {
+            int[] node = q.poll();
             
+            int index = node[0];
+            int sum = node[1];
+
+            if(index == numbers.length - 1){
+                if(sum == target)
+                {
+                    answer++;
+                }
+                continue;  
+            }
+            int nextIndex = index + 1;
+
+            q.add(new int[]{nextIndex, sum + numbers[nextIndex]});
+            q.add(new int[]{nextIndex, sum - numbers[nextIndex]});
         }
 
         return answer;
