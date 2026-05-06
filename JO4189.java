0c805cb9083931544d2c206fb8d4cda01aaa09df
배준석 <qownstjr77@gmail.com>
2026-05-06 22:41:35 +0900

    JO4189

---
+++ JO4189.java
@@ -0,0 +1,9 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class JO4189 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+    }
+}
