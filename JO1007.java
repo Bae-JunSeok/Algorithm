0862bb7042f57b7774c2db4f0954d11bff0b4f63
배준석 <qownstjr77@gmail.com>
2026-05-19 23:27:12 +0900

    JO2548

--- JO1007.java
+++ JO1007.java
@@ -3,6 +3,8 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class JO1007 {
+    // 일단 날짜가 없는 날짜이면 에러를 출력하도록
+    // 날짜를 다 더한 다음에 나누는 방향으로 해야하지 않나??????
     public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
@@ -13,7 +15,11 @@ public class JO1007 {
             int year = 0;
             int month = 0;
             int day = 0;
-            // 만약에 없는 날짜이면 
+            for(int i = 2000; i < year; i++){
+                for(int j = 1; j < month; j++){
+
+                }
+            }
         }
 
         System.out.println(sb);
