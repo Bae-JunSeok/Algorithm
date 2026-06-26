171c66be52d209952a2a13dc69e6daeb1ce305f8
배준석 <qownstjr77@gmail.com>
2026-06-26 19:25:17 +0900

    JO10903

---
+++ JO10903.java
@@ -0,0 +1,9 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class JO10903 {
+    public static void main(String[] args) throws Exception {
+        System.out.println("The world says hello!");
+    }
+}
