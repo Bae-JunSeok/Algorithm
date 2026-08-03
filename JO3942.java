b9751b222b2afa5bc3b00cc0f6971facc5c068a0
배준석 <qownstjr77@gmail.com>
2026-08-03 20:38:26 +0900

    JO3942

--- JO3942.java
+++ JO3942.java
@@ -6,8 +6,8 @@ import java.util.StringTokenizer;
 
 public class JO3942 {
     static int t, a, b;
-    static int[] map;
-    static boolean[] isVisited;
+    static int[][] map;
+    
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -16,49 +16,66 @@ public class JO3942 {
         a = Integer.parseInt(st.nextToken());
         b = Integer.parseInt(st.nextToken());
 
-        map = new int[t + 1];
-        isVisited = new boolean[t + 1];
+        map = new int[2][t + 1];
 
         bfs(0);
 
-        int result = 0;
-
-        for(int i = 1; i <= t; i++){
-            System.out.println(isVisited[i]);
-            if(isVisited[i]){
-                result = i;
+        for(int i = t; i >= 1; i--){
+            if(map[0][i] > 0 || map[1][i] > 0){
+                System.out.println(i);
+                break;
             }
         }
 
-        System.out.println(result);
     }
 
     static void bfs(int start){
-        Queue<Integer> q = new LinkedList<>();
-        q.add(start);
-        isVisited[start] = true;
+        Queue<int[]> q = new LinkedList<>();
+        q.add(new int[]{start, 0});
+        map[0][start] = 0;
 
         while (!q.isEmpty()) {
-            int node = q.poll();
+            int[] node = q.poll();
 
-            int next1 = node + a;
-            if(next1 <= t && !isVisited[next1]){
-                q.add(next1);
-                isVisited[next1] = true;
+            int curX = node[0];
+            int water = node[1];
+
+            int next1 = curX + a;
+            if(next1 <= t && map[water][next1] == 0){
+                q.add(new int[]{next1, water});
+                map[water][next1] = 1;
             }
+
+            int next2 = curX + b;
+            if(next2 <= t && map[water][next2] == 0){
+                q.add(new int[]{next2, water});
+                map[water][next2] = 1;
+            }
+
+            if(water == 0){
+                int next3 = curX / 2;
+                q.add(new int[]{next3, 1});
+                map[1][next3] = 1;
+            }
+
+            // int next1 = node + a;
+            // if(next1 <= t && !isVisited[next1]){
+            //     q.add(next1);
+            //     isVisited[next1] = true;
+            // }
             
 
-            int next2 = node + b;
-            if(next2 <= t && !isVisited[next2]){
-                q.add(next2);
-                isVisited[next2] = true;
-            }
+            // int next2 = node + b;
+            // if(next2 <= t && !isVisited[next2]){
+            //     q.add(next2);
+            //     isVisited[next2] = true;
+            // }
 
-            int next3 = node / 2;
-            if(next3 > 1 && !isVisited[next3]){
-                q.add(next3);
-                isVisited[next3] = true;
-            }
+            // int next3 = node / 2;
+            // if(next3 > 1 && !isVisited[next3]){
+            //     q.add(next3);
+            //     isVisited[next3] = true;
+            // }
         }
     }
 }
