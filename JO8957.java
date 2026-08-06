25aea626a68a9158d279f1913e74c1d1af504f3a
배준석 <qownstjr77@gmail.com>
2026-08-06 21:23:33 +0900

    JO8957

---
+++ JO8957.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class JO8957 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n, m;
+        n = Integer.parseInt(br.readLine());
+        m = Integer.parseInt(br.readLine());
+        System.out.println(n * m);
+
+    }
+}
